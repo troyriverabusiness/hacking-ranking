c591148e52bdf534424b4978e845122a250ee3f8
troy <troyriverabusiness@gmail.com>
2026-02-01 00:51:26 +0100

    cleanup refactor

--- src/lib/supabase/index.ts
+++ src/lib/supabase/index.ts
@@ -1,3 +1,5 @@
+export { signIn, signUp, signOut, getCurrentUser, getSession } from './auth';
+
 export { getAllHackathons, getHackathon, createHackathon, updateHackathon } from './hackathon';
 export { getAllProfiles, getProfile, getRankHistory, checkUsernameAvailability, createProfile, updateProfile } from './profile';
 export { getTeamParticipants } from './team';
